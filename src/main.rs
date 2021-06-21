797a4b849ca810974779e9f4ef2fe4b80fafcdf3
xasopheno <danny@xasopheno.com>
2021-06-21 15:43:38 -0400

    name

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,11 @@ pub enum ScopError {
 
 type NormalForm = i32;
 type Def<T> = HashMap<String, T>;
-#[derive(Debug, Clone)]
-struct Scopes(Vec<String>);
 
 #[derive(Debug, Clone)]
 struct Defs<T> {
     defs: HashMap<String, Def<T>>,
-    scopes: Scopes,
+    scopes: Vec<String>,
 }
 
 fn main() -> Result<(), ScopError> {
@@ -26,7 +24,7 @@ fn main() -> Result<(), ScopError> {
     defs.insert("global", "1", 1)?;
     defs.insert("global", "2", 2)?;
 
-    let new_scope = defs.create_scope();
+    let new_scope = defs.create_uuid_scope();
     defs.insert(&new_scope, "3", 3)?;
 
     let result = defs.find("id");
@@ -34,15 +32,6 @@ fn main() -> Result<(), ScopError> {
     dbg!(result);
 
     Ok(())
-}
-
-impl Scopes {
-    pub fn new() -> Self {
-        Self(vec!["global".to_string()])
-    }
-    pub fn push(&mut self, v: String) {
-        self.0.push(v);
-    }
 }
 
 impl<T> Defs<T>
@@ -54,15 +43,20 @@ where
         defs.insert("global".into(), HashMap::new());
         Self {
             defs: defs,
-            scopes: Scopes::new(),
+            scopes: vec!["global".to_string()],
         }
     }
 
-    pub fn create_scope(&mut self) -> String {
+    pub fn create_uuid_scope(&mut self) -> String {
         let new_scope = Uuid::new_v4().to_string();
         self.defs.insert(new_scope.to_string(), Def::new());
         self.scopes.push(new_scope.to_string());
         new_scope
+    }
+
+    pub fn create_named_scope(&mut self, new_scope: &str) -> () {
+        self.defs.insert(new_scope.to_string(), Def::new());
+        self.scopes.push(new_scope.to_string());
     }
 
     pub fn insert(&mut self, scope: &str, name: &str, value: T) -> Result<(), ScopError> {
@@ -75,7 +69,7 @@ where
     }
 
     pub fn find(&self, id: &str) -> Option<T> {
-        for scope in self.scopes.0.iter().rev() {
+        for scope in self.scopes.iter().rev() {
             let current = self
                 .defs
                 .get(&scope.to_string())
@@ -95,43 +89,46 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_can_insert_and_find_in_global_scope() {
+    fn it_can_insert_and_find_in_global_scope() -> Result<(), ScopError> {
         let mut defs: Defs<NormalForm> = Defs::new();
 
-        defs.insert("global", "1", 1).unwrap();
-        defs.insert("global", "2", 2).unwrap();
+        defs.insert("global", "1", 1)?;
+        defs.insert("global", "2", 2)?;
 
         let found = defs.find("1");
         assert_eq!(found, Some(1));
         let not_found = defs.find("3");
         assert_eq!(not_found, None);
+        Ok(())
     }
 
     #[test]
-    fn it_can_insert_and_find_with_generated_scope_name() {
+    fn it_can_insert_and_find_with_uuid_scope_name() -> Result<(), ScopError> {
         let mut defs: Defs<NormalForm> = Defs::new();
 
-        defs.insert("global", "1", 1).unwrap();
-        defs.insert("global", "2", 2).unwrap();
+        defs.insert("global", "1", 1)?;
+        defs.insert("global", "2", 2)?;
 
-        let new_scope = defs.create_scope();
-        defs.insert(&new_scope, "3", 3).unwrap();
+        let new_scope = defs.create_uuid_scope();
+        defs.insert(&new_scope, "3", 3)?;
 
         let found = defs.find("3");
         assert_eq!(found, Some(3));
+        Ok(())
     }
 
     #[test]
-    fn it_finds_value_in_inner_scope() {
+    fn it_finds_value_in_innermost_scope() -> Result<(), ScopError> {
         let mut defs: Defs<NormalForm> = Defs::new();
 
-        defs.insert("global", "1", 1).unwrap();
-        defs.insert("global", "2", 2).unwrap();
+        defs.insert("global", "1", 1)?;
+        defs.insert("global", "2", 2)?;
 
-        let new_scope = defs.create_scope();
-        defs.insert(&new_scope, "1", 10).unwrap();
+        let new_scope = defs.create_uuid_scope();
+        defs.insert(&new_scope, "1", 10)?;
 
         let found = defs.find("1");
         assert_eq!(found, Some(10));
+        Ok(())
     }
 }
